92b4f2e2f5ae825b5d92ba297428c27eaf4e9d95
Chi-Liang, Liu <liangtaiwan1230@gmail.com>
2020-03-17 20:27:24 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# google-drive-downloader
+A script for download google-drive data (including large file)
---
+++ drive_downloader.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+id=$1
+output=$2
+
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id='$id -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$id" -O $output && rm -rf /tmp/cookies.txt
